4579c32923b5f458b52e7ac1ffb9abd6dbb61b37
syllogismos <anilkaraka@live.in>
2017-03-16 05:19:35 +0530

    sarsa and policy gradient

--- sutton_exercises/10/MountainCar.py
+++ sutton_exercises/10/MountainCar.py
@@ -14,13 +14,17 @@ from gym import wrappers
 from TileCoding import IHT, tiles
 import random
 from tqdm import tqdm
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+
 # from mountain_car import MountainCarEnv
 
 import pickle
 
 NUM_OF_TILINGS = 8
 MAX_SIZE = 2014
-MAX_EPISODES = 1200
+MAX_EPISODES = 20000
 EPSILON = 0.1
 ALPHA = 0.01
 GAMMA = 0.98
@@ -34,61 +38,69 @@ iht = IHT(MAX_SIZE)
 # env = gym.make('MountainCar-v0')
 weights = np.zeros(MAX_SIZE)
 
-def get_value(state, action, weights, env):
-    position = state[0]
-    velocity = state[1]
-    max_position, max_velocity = tuple(env.observation_space.high)
-    min_position, min_velocity = tuple(env.observation_space.low)
-    active_tiles = tiles(iht, NUM_OF_TILINGS, [8*position/(max_position - min_position), 8*velocity/(max_velocity - min_velocity)], [action])
-    return sum(weights[active_tiles])  # + 0.5*reg*sum(weights * weights)
+def get_epsilon(iteration):
+    if iteration < 100:
+        return 0.2
+    elif iteration < 5000:
+        return 0.1
+    elif iteration < 10000:
+        return 0.01
+    else: return 0.001
+
+def get_q_value(state, action, weights, env):
+    active_tiles = get_active_tiles(state, action, env)
+    # active_tiles = tiles(iht, NUM_OF_TILINGS, [8*position/(max_position - min_position), 8*velocity/(max_velocity - min_velocity)], [action])
+    return sum(weights[active_tiles]), active_tiles  # + 0.5*reg*sum(weights * weights)
 
 def get_active_tiles(state, action, env):
     position = state[0]
     velocity = state[1]
-    max_position, max_velocity = tuple(env.observation_space.high)
-    min_position, min_velocity = tuple(env.observation_space.low)
-    return tiles(iht, NUM_OF_TILINGS, [8*position/(max_position - min_position), 8*velocity/(max_velocity - min_velocity)], [action])
+    x3 = state[2]
+    x4 = state[3]
+    max_position, max_velocity, max_x3, max_x4 = tuple(env.observation_space.high)
+    min_position, min_velocity, min_x3, min_x4 = tuple(env.observation_space.low)
+    return tiles(iht, NUM_OF_TILINGS, [8*position/(max_position - min_position),
+                                       8*velocity/(max_velocity - min_velocity),
+                                       8*x3/(max_x3 - min_x3),
+                                       8*x4/(max_x4 - min_x4)], [action])
 
-def pick_epsilon_greedy_action(state, weights, env):
-    actions = [0,1,2]
-    if random.random() < EPSILON:
+def pick_epsilon_greedy_action(state, weights, env, i):
+    actions = [0,1]
+    if random.random() < get_epsilon(i):
         return random.choice(actions)
     else:
-        state_action_values = map(lambda a: get_value(state, a, weights, env), actions)
+        state_action_values = map(lambda a: get_q_value(state, a, weights, env)[0], actions)
         max_value = max(state_action_values)
         return state_action_values.index(max_value)
 
 def pick_best_action(state, weights, env):
-    actions = [0,1,2]
-    state_action_values = map(lambda a: get_value(state, a, weights, env), actions)
+    actions = [0,1]
+    state_action_values = map(lambda a: get_q_value(state, a, weights, env)[0], actions)
     max_value = max(state_action_values)
     return state_action_values.index(max_value)
     
-def semi_gradient_sarsa():
+def semi_gradient_sarsa(env, upload=False):
     weights = np.zeros(MAX_SIZE)
-    env = gym.make('MountainCar-v0')
-    env._max_episode_steps = 50000
-    # env = MountainCarEnv()
-    # env = wrappers.Monitor(env, OUTDIR, force = True)
-    for i in range(MAX_EPISODES):
+    # env = gym.make('MountainCar-v0')
+    # env._max_episode_steps = 50000
+    if upload:
+        env = wrappers.Monitor(env, OUTDIR, force = True)
+    for i in tqdm(range(MAX_EPISODES)):
         state = env.reset()
-        action = pick_epsilon_greedy_action(state, weights, env)
+        action = pick_epsilon_greedy_action(state, weights, env, i)
         losses = []
+        episode_length = 0
         while True:
+            episode_length += 1
             next_state, reward, done, info = env.step(action)
-            # print done, next_state, reward, action
             env.render()
-            active_tiles = get_active_tiles(state, action, env)
-            state_value = get_value(state, action, weights, env)
+            state_value, active_tiles = get_q_value(state, action, weights, env)
             if done:
-                # print sum(weights), 'weights sum'
-                # print done, info, next_state, reward
-                # print "its done it seems", i
                 delta = reward - state_value
                 weights[active_tiles] += ALPHA * delta
                 break
-            next_action = pick_epsilon_greedy_action(next_state, weights, env)
-            next_state_value = get_value(next_state, next_action, weights, env)
+            next_action = pick_epsilon_greedy_action(next_state, weights, env, i)
+            next_state_value = get_q_value(next_state, next_action, weights, env)[0]
             delta = reward + (GAMMA * next_state_value) - state_value
             loss = delta**2
             losses.append(loss)
@@ -96,16 +108,38 @@ def semi_gradient_sarsa():
             # weights -= reg*weights
             state = next_state
             action = next_action
-        print np.mean(losses), sum(weights)
+        # print np.mean(losses), sum(weights)
+        if (i+1)%100 == 0:
+            print episode_length
         losses = []
 
     env.close()
-    # gym.upload(OUTDIR)
+    if upload:
+        gym.upload(OUTDIR)
     # pickle.dump(weights, open('MountainCar_v0_weights_120eps.pkl', 'wb'))
     return weights
 
-
+def plot_cost_to_go_mountain_car(env, weights, num_tiles=20):
+    x = np.random.uniform(env.observation_space.low[0], env.observation_space.high[0], num_tiles)
+    y = np.random.uniform(env.observation_space.low[1], env.observation_space.high[1], num_tiles)
+    X, Y = np.meshgrid(x, y)
+    # Z = np.apply_along_axis(lambda _: -np.max(estimator.predict(_)), 2, np.dstack([X, Y]))
+    Z = np.zeros(len(x))
+    for i in range(len(x)):
+        Z[i] = pick_best_action([x[i], y[i]], weights, env)
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                           cmap=matplotlib.cm.coolwarm, vmin=-1.0, vmax=1.0)
+    ax.set_xlabel('Position')
+    ax.set_ylabel('Velocity')
+    ax.set_zlabel('Value')
+    ax.set_title("Mountain \"Cost To Go\" Function")
+    fig.colorbar(surf)
+    plt.show()
 
 if __name__ == '__main__':
     # Training sarsa weights
-    weights = semi_gradient_sarsa()
+    env = gym.make('CartPole-v0')
+    weights = semi_gradient_sarsa(env, True)
+    # plot_cost_to_go_mountain_car(env, weights, 250)
